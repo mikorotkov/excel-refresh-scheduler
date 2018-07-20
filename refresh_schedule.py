a6154891e9350d86281dad0edfcdf6916052ec1e
Michael Korotkov <michael.korotkov@experteer.com>
2018-07-20 16:00:42 +0200

    added error catching on save

--- refresh_schedule.py
+++ refresh_schedule.py
@@ -3,28 +3,37 @@ import time
 import win32com.client
 
 def refresh_files(file_list,time_now):
+    error_list=[]
     for file in file_list:
 
         # Start an instance of Excel
+        print(file)
         xlapp = win32com.client.DispatchEx("Excel.Application")
 
-    # Open the workbook in said instance of Excel
+        # Open the workbook in said instance of Excel
         wb = xlapp.workbooks.open(file)
 
-    # Optional, e.g. if you want to debug
-    # xlapp.Visible = True
+        # Optional, e.g. if you want to debug
+        xlapp.Visible = True
+        print('Working on file {}'.format(file))
 
-    # Refresh all data connections.
+        # Refresh all data connections.
         wb.Model.Refresh()
         wb.RefreshAll()
         xlapp.CalculateUntilAsyncQueriesDone()
-        wb.Save()
+        try:
+            wb.Save()
+            print('Saved')
+        except: 
+            error_list.append(file)
+            print('File was not saved')
+            continue
         wb.Close(True)
+            
 
-        print("All is done")
-        # Quit
-        xlapp.Quit()
-        print("I'm working...")
+    # Quit
+    xlapp.Quit()
+
 
 
 
@@ -41,4 +50,5 @@ def refresh_files(file_list,time_now):
     #schedule.run_pending()
 cur_time = time.localtime(time.time())
 time_now=str(cur_time[3]) + ':' + str(cur_time[4])
+refresh_files(['Y:\\Draft - CA dashboard v2.xlsx'],time_now)
 
