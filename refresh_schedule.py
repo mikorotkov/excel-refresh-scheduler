309d61d03035b6a1691458b2830ba586bf49189d
Michael Korotkov <fezuhe@hotmail.com>
2018-08-10 21:46:23 +0200

    update links message is not shown at the beginning anymore

--- refresh_schedule.py
+++ refresh_schedule.py
@@ -1,32 +1,37 @@
 import schedule
 import time
 import win32com.client
-#import shutil
 
 def daily_bi_queries():
 # Open Excel
- #   Application = win32com.client.Dispatch("Excel.Application")
+    path='C:\\Users\\Michael\\Downloads\\'
+    FileName='source_file_Austria.xlsx'
+    Application = win32com.client.Dispatch("Excel.Application")
  
-# Show Excel. While this is not required, it can help with debugging
- #   Application.Visible = 1
- 
-# Open Your Workbook
- #   Workbook = Application.Workbooks.open(SourcePathName + '/' + FileName)
- 
-# Refesh All
-  #  Workbook.RefreshAll()
- 
-# Saves the Workbook
- #   Workbook.Save()
- 
-# Closes Excel
- #   Application.Quit()
-    print("I'm working...")
-    print(f'SourcePathName is {SourcePathName} and FileName is {FileName}')
+ # Show Excel. While this is not required, it can help with debugging
+    Application.Visible = 1
+    Application.DisplayAlerts=False
+    Application.AskToUpdateLinks = False
+ # Open Your Workbook
+    Workbook = Application.Workbooks.open(path + FileName)
+    #try:
+    #    Workbook.UpdateLink(Name=Workbook.LinkSources())
 
-schedule.every(1).minutes.do(daily_bi_queries())
+   # except Exception as e:
+    #    print(e)
+    # Refesh All
+    Workbook.RefreshAll()
+    print('Done')
+ # Saves the Workbook
+    Workbook.Save()
+ 
+ # Closes Excel
+    Application.Quit()
+
+
+#schedule.every(1).minutes.do(daily_bi_queries)
 #schedule.every().hour.do(job)
-#schedule.every().day.at("10:30").do(job)
+#schedule.every().day.at("10:30").do(daily_bi_queries)
 #schedule.every().monday.do(job)
 #schedule.every().wednesday.at("13:15").do(job)
 
